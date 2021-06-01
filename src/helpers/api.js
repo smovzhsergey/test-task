86b72936a9fb9294c186f403e9b424cf00dcd3f6
Smovzh Sergey <smovzh.sergey@gmail.com>
2021-06-01 18:07:19 +0300

    fix api address

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,1 +1,4 @@
-export const apiUrl = 'https://api.dev.buki.com.ua/frontend_test';
+// export const apiUrl = 'https://api.dev.buki.com.ua/frontend_test';
+// secret delete link
+// https://designer.mocky.io/manage/delete/7a0f030b-5909-49dd-b3cd-fa2ae727600a/pbSMiGRIFuhzX9Rik0uCSJgPAsUyvwjVwpkf
+export const apiUrl = 'https://run.mocky.io/v3/7a0f030b-5909-49dd-b3cd-fa2ae727600a';
